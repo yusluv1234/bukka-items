d883031937caf81b16fc22dd19546fee8d24ad48
yusluv1234 <yusluv124@gamil.com>
2020-10-26 18:21:13 +0100

    'done'

--- src/components/sidebarMin.js
+++ src/components/sidebarMin.js
@@ -5,7 +5,8 @@ import History from './images/history.svg';
 import Menu from './images/menu.svg';
 import savedCart from './images/savecart.svg';
 import Settings from './images/settings.svg';
-import contactSupport from './images/contact.svg'
+import contactSupport from './images/contact.svg';
+import Plus from './images/plus.svg';
 
 import './sidebarMin.css';
 
@@ -18,16 +19,18 @@ const SidebarMin = () => {
     const icone = <img src={savedCart} alt='instore' />;
     const icong = <img src={Settings} alt='instore' />;
     const iconh = <img src={contactSupport} alt='instore' />;
+    const iconi = <img src={Plus} alt='instore' />;
 
     return (
         <div className='sidebar-min'>
+                <p>{iconi}</p> <br />
                 <p>{icona}</p><br />
                 <p>{iconb}</p><br />
                 <p>{iconc}</p><br />
                 <p>{icond}</p><br />
                 <p>{icone}</p><br />
                 <p>{icong}</p><br />
-                <p>{iconh}</p>
+                <p>{iconh}</p> 
         </div>
     )
 }
